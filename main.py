1c4a69489fdf2895652b2be7a336277031e3935d
João Paulo Leôncio <joaopauloleoncio2002@gmail.com>
2025-07-03 01:28:22 -0300

    main.py

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 import tkinter as tk
 from tkinter import messagebox
+from calculos import calcular_preco  # Importa a função do outro arquivo
 
 janela_ajustada = False  # Flag para controlar crescimento da janela só na primeira vez
-
-def calcular_preco(horas, valor_hora, peso_rolo, valor_rolo,
-                   linha_usada, margem, papel, brinde, tag, cartoes):
-
-    custo_mao_obra = horas * valor_hora
-    custo_linha = (linha_usada / peso_rolo) * valor_rolo if peso_rolo != 0 else 0
-    custo_extra = papel + brinde + tag + cartoes
-    custo_total = custo_mao_obra + custo_linha + custo_extra
-    preco_final = custo_total * (1 + margem / 100)
-
-    return {
-        "custo_mao_obra": custo_mao_obra,
-        "custo_linha": custo_linha,
-        "custo_extra": custo_extra,
-        "custo_total": custo_total,
-        "preco_final": preco_final
-    }
 
 def str_para_float(valor_str):
     try:
